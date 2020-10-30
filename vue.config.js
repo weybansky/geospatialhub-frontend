a9ef4f13e2326ee853095646d15bb5e1c289ac3e
Abdulwahab Nasir <weybansky@gmail.com>
2020-10-30 17:24:13 +0100

    vue pwa config--due to netlify _redirect issues

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pwa: {
+    name: "The Geospatial Hub",
+    themeColor: "#0e67ec",
+    msTileColor: "#0e67ec",
+    appleMobileWebAppCapable: "no",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    manifestOptions: {
+      short_name: "GeosptatialHub"
+    },
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      //   // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      //   // ...other Workbox options...
+      exclude: [/_redirects/]
+      // exclude: [/\.map$/, /_redirects/],
+    }
+  }
+};
