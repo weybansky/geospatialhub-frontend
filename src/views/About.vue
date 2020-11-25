c6f54070d975fa66ced41f42add299d7a0935410
Abdulwahab Nasir <weybansky@gmail.com>
2020-11-25 04:57:28 +0100

    before design changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
+  <div class="about-page">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import "../scss/about-page";
+</style>
