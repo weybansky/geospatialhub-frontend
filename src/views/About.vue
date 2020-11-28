8464fc5bf941b82e8217bead02b215af5b171dcd
Abdulwahab Nasir <weybansky@gmail.com>
2020-11-28 05:34:39 +0100

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,49 @@
 <template>
   <div class="about-page">
-    <h1>This is an about page</h1>
+    <div class="page-title">
+      <h1 class="title">About</h1>
+    </div>
+
+    <main class="body">
+      <img src="course_image.png" alt="" />
+      <p class="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        nemo voluptatem deserunt quam explicabo perspiciatis cum. Laudantium
+        aliquid dolor, necessitatibus quidem explicabo obcaecati laborum! Quod
+        dolore reiciendis quis nam praesentium qui provident iusto soluta
+        distinctio, veritatis in perferendis quos repellendus numquam
+        dignissimos, impedit quo alias, fugit odio. Dignissimos, reprehenderit
+        perspiciatis adipisci, maiores repellendus vel beatae, quibusdam neque
+        alias corrupti doloribus illo voluptatibus a modi deserunt distinctio
+        asperiores. Fugiat doloribus amet error obcaecati nobis ad rem
+        exercitationem sint quae atque sit, maxime esse recusandae beatae quis
+        possimus facere laborum eum inventore optio eos enim neque debitis
+        maiores. Quae non voluptatum aliquid sed nobis repudiandae tenetur eius,
+        dicta exercitationem minima commodi laboriosam, minus eos rem vitae
+        libero modi impedit. Quis magni similique a laborum ex nobis cum
+        adipisci odit maxime? Ut quam consectetur magnam atque non, vel,
+        provident minus veniam, esse dignissimos quae molestias quo. Fuga esse
+        ea, quasi adipisci magnam similique!
+      </p>
+      <p class="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+        perferendis a voluptatibus modi ex voluptates, illum cupiditate
+        similique dolor assumenda, eaque incidunt repellat tempora cum odio
+        vero. Asperiores alias aut quia nihil, modi ipsam cumque labore quisquam
+        tempore possimus voluptatem qui ea maiores natus quae, optio impedit,
+        laborum earum fugit blanditiis. Omnis laboriosam, accusantium quam
+        libero dolorem rerum quasi nulla sit consequatur quisquam exercitationem
+        repellat nobis facere dolore error reprehenderit tempora modi officiis!
+        Soluta vitae at fugiat. Recusandae quia quidem ea id. Esse, quis
+        distinctio corporis, exercitationem laudantium delectus corrupti quam
+        tenetur culpa adipisci eum. Modi commodi laboriosam, maiores obcaecati
+        cumque, sequi veritatis quisquam quam nulla corporis quo iure
+        voluptatibus, dignissimos corrupti. Dolorem, dignissimos. Debitis,
+        cumque, voluptatem quos laborum fuga officia ut, fugit maxime hic
+        commodi sit soluta. Blanditiis, porro reprehenderit repudiandae, unde
+        incidunt perferendis praesentium facere sit aspernatur dolor, commodi
+        ratione voluptas soluta ut quasi deleniti eveniet quisquam cumque.
+      </p>
+    </main>
   </div>
 </template>
